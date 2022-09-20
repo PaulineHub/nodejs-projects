33f50344622064102d6fce41c85499b6dff490bf
Pauline <huby.pauline@gmail.com>
2022-09-19 20:10:05 -0400

    add course 6 job api

--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -4,6 +4,7 @@ const {BadRequestError} = require('../errors')
 const login = async (req,res) => {
     const {username,password} = req.body
 
+    // check for empty values
     if(!username || !password){
         throw new BadRequestError('Please provide email and password')
     }
@@ -11,6 +12,9 @@ const login = async (req,res) => {
     //just for demo, normally provide by DB
     const id = new Date().getDate()
     //method jwt.sign()
+    // send payload (object)(DONT PUT A PASSWORD IN IT), a secret string, and options
+    // try to keep payload small for better user experience
+    // in production, the secret string must be long and unguessable
     const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
 
     res.status(200).json({msg:'user created',token})
---
+++ 06-jobs-api/controllers/auth.js
@@ -0,0 +1,44 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes') //library installed from packaje.json
+const { BadRequestError, UnauthenticatedError } = require('../errors')
+
+const register = async (req, res) => {
+
+  //mongoose .create() method save one or more documents to the database.
+  // https://mongoosejs.com/docs/api.html#model_Model-create
+
+  // {...req.body} : decomposition avec litteraux d'objet
+  // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+  // var profil = { prenom: 'Sarah', profilComplet: false };
+  // var profilMisAJour = { nom: 'Dupont', profilComplet: true };
+  // var fusion = { ...profil, ...profilMisAJour };
+  // Object { prenom: 'Sarah', nom: 'Dupont', profilComplet: true };
+
+  const user = await User.create({ ...req.body })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
+}
+
+const login = async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+  // compare password
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
+}
+
+module.exports = {
+  register,
+  login,
+}
